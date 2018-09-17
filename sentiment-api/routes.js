bad4afb43a3a7f81e71ae415bb8e6ecec7d1c1ed
Nathan Lapinski <n4t311@gmail.com>
2018-09-17 13:59:54 +0900

    Make YouTube its own module
    Refactors youtube-comments.js to export
    a module with two public functions, one for fetching
    raw response data, the other for returning only
    an array of comments. Adds basic error handling.
    
    Updates routes.js to use the new Youtube module.

--- sentiment-api/routes.js
+++ sentiment-api/routes.js
@@ -1,22 +1,25 @@
 var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 
+var youtube = require('../sentiment-api/third_party_apis/youtube/youtube-comments');
 
 var appRouter = function (app) {
-  app.post("/sentiment", function(req, res) {
-    
-    console.log(req.body);
-    if (req.body.comments) {
 
-        const results = req.body.comments.map(c => sentiment.analyze(c));
-
-        res.status(200).send(results);
+  // Sentiment endpoint for Youtube comments. Other endpoints can follow a similar convention
+  // for instance, sentiment/tw for something like Twitter.
+  app.get("/sentiment/yt", function(req, res) {
+    if (req.query.id) {
+        youtube.getCommentsTextById(req.query.id)
+               .then(comments => {
+                 const results = comments.map(c => sentiment.analyze(c));
+                 res.status(200).send(results);
+               }).catch(err => res.status(500).send(`Error getting comment sentiment: ${err}`));
     } else {
-        res.status(200).send('No Query specified: ' + sentiment.analyze('Cats are stupid.'));
+        res.status(200).send([]);
     }
   });
 
-  app.get("/", function(req, res) {
+  app.get("/*", function(req, res) {
     res.status(200).send('Go to /sentiment to do stuff');
   });
 }
---
+++ sentiment-api/third_party_apis/youtube/youtube-comments.js
@@ -0,0 +1,47 @@
+require('dotenv/config');
+const axios = require('axios');
+const ApiKey = process.env.API_KEY; //TODO: set an env variable on heroku
+const youtubeUrl = 'https://www.googleapis.com/youtube/v3/commentThreads';
+
+// private
+// helper function to filter a raw response into an array of comments
+const createCommentsArray = (res) => {
+  let commentsArray = [];
+  // by default  youtube data api send 20 comments back
+  if (res.data && res.data.pageInfo && res.data.pageInfo.totalResults) {
+    for(let i = 0; i < res.data.pageInfo.totalResults ; i++) {
+      commentsArray.push(res.data.items[i].snippet.topLevelComment.snippet.textDisplay);
+    }
+  }
+  return commentsArray;
+}
+
+// private function. hits Youtube's api and returnd the comments for a given video id
+const fetch = (videoId) => {
+  return axios.get(youtubeUrl, {
+    params : {
+      part: 'snippet',
+      videoId: videoId,
+      key: ApiKey
+    }
+  });
+}
+
+// public
+// returns the raw response object from the YouTube api
+// returns [] by default, or in case of error.
+const getCommentsById = (videoId) => {
+  return fetch(videoId).catch(() => []);
+}
+
+// public
+// returns only the comment strings for a video
+// returns [] by default, or in case of error.
+const getCommentsTextById = (videoId) => {
+  return fetch(videoId).then(commentObjects => createCommentsArray(commentObjects)).catch(() => []);
+}
+
+module.exports =  {
+  getCommentsById,
+  getCommentsTextById
+};
